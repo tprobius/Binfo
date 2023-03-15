3c59396afd25b6da0ff732abf5eaa724ba27d244
tprobius <dviktoriyas@gmail.com>
2023-03-15 19:40:02 +0700

    Adding HomeScreen

--- app/src/main/java/com/tprobius/binformation/MainActivity.kt
+++ app/src/main/java/com/tprobius/binformation/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
+import com.tprobius.binformation.ui.screens.homescreen.HomeScreen
 import com.tprobius.binformation.ui.theme.BinformationTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +20,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-
+                    HomeScreen()
                 }
             }
         }
---
+++ app/src/main/java/com/tprobius/binformation/ui/screens/homescreen/HomeScreen.kt
@@ -0,0 +1,8 @@
+package com.tprobius.binformation.ui.screens.homescreen
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HomeScreen() {
+
+}
