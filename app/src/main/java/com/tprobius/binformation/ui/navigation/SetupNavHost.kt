1bf442829cd0009b986037c9956a7b7f1340b199
tprobius <dviktoriyas@gmail.com>
2023-03-15 23:47:55 +0700

    Adding SetupNavHost

---
+++ app/src/main/java/com/tprobius/binformation/ui/navigation/SetupNavHost.kt
@@ -0,0 +1,29 @@
+package com.tprobius.binformation.ui.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.tprobius.binformation.ui.screens.homescreen.HomeScreen
+
+sealed class Screens(val route: String) {
+    object HomeScreen : Screens(route = "home_screen")
+    object HistoryScreen : Screens(route = "history_screen")
+}
+
+@Composable
+fun SetupNavHost(navController: NavHostController) {
+    NavHost(
+        navController = navController,
+        startDestination = Screens.HomeScreen.route
+    ) {
+        composable(route = Screens.HomeScreen.route) {
+            HomeScreen(navController = navController)
+        }
+
+        composable(route = Screens.HistoryScreen.route)
+        {
+//            HistoryScreen(navController = navController, it.arguments?.getString("id"))
+        }
+    }
+}
