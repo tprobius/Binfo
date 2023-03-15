9e1d4de5b8ad02067f83fbe683b968289df349b6
tprobius <dviktoriyas@gmail.com>
2023-03-15 20:01:24 +0700

    Updating HomeScreenViewModel

--- app/src/main/java/com/tprobius/binformation/ui/screens/homescreen/HomeScreenViewModel.kt
+++ app/src/main/java/com/tprobius/binformation/ui/screens/homescreen/HomeScreenViewModel.kt
@@ -1,4 +1,25 @@
 package com.tprobius.binformation.ui.screens.homescreen
 
-class HomeScreenViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.tprobius.binformation.data.api.model.Binformation
+import com.tprobius.binformation.data.repository.BinformationRepository
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+class HomeScreenViewModel @Inject constructor(
+    private val binformationRepository: BinformationRepository
+) : ViewModel() {
+    private val _state = MutableLiveData<Binformation>()
+    val state: LiveData<Binformation>
+        get() = _state
+
+    init {
+        viewModelScope.launch {
+            val binformation = binformationRepository.getBinformation()
+            _state.value = binformation
+        }
+    }
 }
