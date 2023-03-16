52045ea739d802429efb421ddc159f7ec0ecf989
tprobius <dviktoriyas@gmail.com>
2023-03-16 20:41:33 +0700

    Fix for 'java.security.cert.CertPathValidatorException: Trust anchor for certification path not found' error

--- app/src/main/java/com/tprobius/binformation/di/BinformationApiModule.kt
+++ app/src/main/java/com/tprobius/binformation/di/BinformationApiModule.kt
@@ -1,14 +1,19 @@
 package com.tprobius.binformation.di
 
+import android.annotation.SuppressLint
 import com.tprobius.binformation.data.api.ApiConstants
 import com.tprobius.binformation.data.api.BinformationApi
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import java.security.cert.X509Certificate
 import javax.inject.Singleton
+import javax.net.ssl.*
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -24,8 +29,34 @@ object BinformationApiModule {
     @Provides
     @Singleton
     fun provideRetrofit(): Retrofit.Builder {
+        val logging = HttpLoggingInterceptor()
+        logging.setLevel((HttpLoggingInterceptor.Level.BODY))
+        val client = getUnsafeOkHttpClient()
         return Retrofit.Builder()
             .baseUrl(ApiConstants.BASE_URL)
             .addConverterFactory(MoshiConverterFactory.create())
+            .client(client)
     }
+}
+
+@SuppressLint("TrustAllX509TrustManager")
+private fun getUnsafeOkHttpClient(): OkHttpClient {
+    val trustAllCerts = arrayOf<TrustManager>(@SuppressLint("CustomX509TrustManager")
+    object : X509TrustManager {
+        override fun checkClientTrusted(chain: Array<out X509Certificate>?, authType: String?) {
+        }
+
+        override fun checkServerTrusted(chain: Array<out X509Certificate>?, authType: String?) {
+        }
+
+        override fun getAcceptedIssuers() = arrayOf<X509Certificate>()
+    })
+
+    val sslContext = SSLContext.getInstance("SSL")
+    sslContext.init(null, trustAllCerts, java.security.SecureRandom())
+    val sslSocketFactory = sslContext.socketFactory
+
+    return OkHttpClient.Builder()
+        .sslSocketFactory(sslSocketFactory, trustAllCerts[0] as X509TrustManager)
+        .hostnameVerifier { _, _ -> true }.build()
 }
