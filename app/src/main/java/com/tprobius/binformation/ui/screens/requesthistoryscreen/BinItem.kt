628b48d2587cecf66f901a17ff814673402f675c
tprobius <dviktoriyas@gmail.com>
2023-03-18 15:48:42 +0700

    Adding BinItem

---
+++ app/src/main/java/com/tprobius/binformation/ui/screens/requesthistoryscreen/BinItem.kt
@@ -0,0 +1,52 @@
+package com.tprobius.binformation.ui.screens.requesthistoryscreen
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Card
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.tprobius.binformation.domain.model.Bins
+
+@Composable
+fun BinItem(
+    bin: Bins,
+    modifier: Modifier = Modifier,
+    onDeleteClick: () -> Unit
+) {
+    Card(
+        modifier = modifier,
+        elevation = 5.dp
+    ) {
+        Row(
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(
+                modifier = Modifier
+                    .padding(bottom = 16.dp, start = 16.dp, end = 16.dp),
+                fontSize = 16.sp,
+                maxLines = 1,
+                text = bin.number.toString()
+            )
+            IconButton(
+                onClick = onDeleteClick,
+                modifier = Modifier.align(Alignment.Bottom)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Delete,
+                    contentDescription = "Delete note"
+                )
+            }
+        }
+    }
+
+
+}
