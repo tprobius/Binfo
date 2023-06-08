5ffcf7122c03c9e89a1ac55434dbb8be7fbbb6dc
Заремба Виктория <dviktoriyas@gmail.com>
2023-06-09 00:14:13 +0700

    Unused data class deleted

--- app/src/main/java/com/tprobius/binformation/presentation/screens/historyscreen/BinState.kt
+++
@@ -1,7 +0,0 @@
-package com.tprobius.binformation.presentation.screens.historyscreen
-
-import com.tprobius.binformation.domain.entities.Bin
-
-data class BinState(
-    val bins: List<Bin> = emptyList()
-)
